eb1cef5195b121d78370901f18a7a74b33e5ceed
psj0640 <sijun0640@naver.com>
2022-04-24 21:56:01 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <p><h1> let;s develop management system</h1></p>
+    </div>
+  );
+}
+     
+export default App;
+ 
